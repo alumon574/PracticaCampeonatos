788e5ae6a72792aa22460b824a3b01796470c215
alumon574 <alumon574@ieselcaminas.org>
2022-02-21 20:05:23 +0100

    Initial commit

---
+++ src/com/company/Atleta.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.time.LocalDate;
+
+public class Atleta {
+    private String Nombre;
+    private enum Genero
+        {
+        Masculino,Femenino
+    }
+    private LocalDate Nacimiento;
+    private LocalDate InicioCarrera;
+    private LocalDate FinCarrera;
+
+   /* public static int edad(int edadAtleta){
+        int edad=edadAtleta;
+        return edad;
+    }
+    //public static boolean debutar(LocalDate fecha){
+      //
+    //}
+    public static boolean retirar(LocalDate fecha){
+
+    }
+    public static boolean estaActivo(LocalDate fecha){
+
+    }
+    public static int diasActivo(int debutar, int retirar){
+
+    }*/
+}
+
