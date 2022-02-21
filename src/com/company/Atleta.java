1119660ad2ffd822630556165672c69e8c547e0b
alumon574 <alumon574@ieselcaminas.org>
2022-02-21 20:49:14 +0100

    hecho constructor entero, falta hacer 2 constructores sin los 2 ultimos parametros

--- src/com/company/Atleta.java
+++ src/com/company/Atleta.java
@@ -3,23 +3,28 @@ package com.company;
 import java.time.LocalDate;
 
 public class Atleta {
-    private String Nombre;
-    private enum Genero
-        {
-        Masculino,Femenino
-    }
-    private LocalDate Nacimiento;
-    private LocalDate InicioCarrera;
-    private LocalDate FinCarrera;
+    private String name;
+    private Gender gender;
+    private LocalDate birthDate;
+    private LocalDate careerStart;
+    private LocalDate careerEnd;
 
-   /* public static int edad(int edadAtleta){
-        int edad=edadAtleta;
-        return edad;
+    public Atleta(String name, Gender gender, LocalDate birthDate, LocalDate careerStart, LocalDate careerEnd){
+        this.name=name;
+        this.gender=gender;
+        this.birthDate=birthDate;
+        this.careerStart=careerStart;
+        this.careerEnd=careerEnd;
+
     }
-    //public static boolean debutar(LocalDate fecha){
+
+        /* public static int edad(int edadAtleta){
+
+    }
+    //public static boolean debutar(LocalDate careerStart){
       //
     //}
-    public static boolean retirar(LocalDate fecha){
+    public static boolean retirar(LocalDate careerEnd){
 
     }
     public static boolean estaActivo(LocalDate fecha){
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Atleta atleta1 = new Atleta("Pepe", Gender.MALE, LocalDate.of(1990, 9, 25), LocalDate.of(2002, 9, 25), LocalDate.of(2010, 9, 25));
     }
 }
